9b302688f2cc9ae1fc75427652aa7214f984e6aa
louis baptiste <louisvis@ymail.com>
2020-01-21 22:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (distance){
   let hqLocation= 42;
-  return Math.abs(distance-  hqLocation=42);
+  return Math.abs(distance-hqLocation=42);
 
 
 
