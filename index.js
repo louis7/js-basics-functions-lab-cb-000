cd6bbed1cbb5ccd2e486b4368d44bd1610e14f44
louis baptiste <louisvis@ymail.com>
2020-01-21 22:35:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function distanceFromHqInBlocks (distance){
    hqLocation= 42;
   return Math.abs(distance-hqLocation=42);
 }
+
 function distanceFromHqInFeet(distance){
   return distanceInFeet = distanceFromHqInBlocks (distance) * 264
 }
