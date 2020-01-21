4de346cc02eb8aea8eb79e3225c951ebab813d3d
louis baptiste <louisvis@ymail.com>
2020-01-21 22:40:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks (blockNumber) {
-  if (blockNumber > 42) {
-    return blockNumber - 42;
-  } else {
-    return 42 - blockNumber;
-  }
+function distanceFromHqInBlocks(distance){
+   hqLocation= 42;
+  return Math.abs(distance-hqLocation=42);
 }
 
 function distanceFromHqInFeet(distance){
